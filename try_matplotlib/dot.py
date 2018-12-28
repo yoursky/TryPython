d4e6304e8b75c97922db2d88976abfadc0959c21
Hao Liu <hao.liu@transwarp.io>
2018-12-28 20:29:02 +0800

    Try matplotlib: 画图

---
+++ try_matplotlib/dot.py
@@ -0,0 +1,15 @@
+
+from numpy import *;
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+N = 50 
+x = np.random.rand(N) 
+y = np.random.rand(N) 
+colors = np.random.rand(N) 
+area = np.pi * (15 * np.random.rand(N))**2  
+# plt.scatter(x, y, s=area, c=colors, alpha=0.5, marker='p')
+plt.scatter(x, y, s=area, c=colors, alpha=0.5, marker='x')
+# plt.scatter(x, y, s=area, c=colors, alpha=0.5, marker=',')
+# plt.scatter(x, y, s=area, c=colors, alpha=0.5, marker=(9, 3, 30))
+plt.show()
