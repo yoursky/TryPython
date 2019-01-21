400a41d6acb0378b85ed94205a1e9c21da5a09a8
Hao Liu <yoursky0@163.com>
2019-01-21 09:08:43 +0800

    贪婪算法可以求近似解 + NP问题

---
+++ algorithm/greed.py
@@ -0,0 +1,27 @@
+#coding:utf-8
+
+states_needed = set(['mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az'])
+# 广播清单
+stations = {}
+stations['kone'] = set(['id', 'nv', 'ut'])
+stations['ktwo'] = set(['id', 'wa', 'mt'])
+stations['kthree'] = set(['or', 'nv', 'ca'])
+stations['kfour'] = set(['nv', 'ut'])
+stations['kfive'] = set(['ca', 'az'])
+
+final_station = []
+
+while states_needed:
+    best_station = None
+    best_covered = set()
+    for station, states in stations.items():
+        covered = states_needed & states
+        if len(covered) > len(best_covered):
+            best_station = station
+            best_covered = covered
+    
+    states_needed -= best_covered
+    # stations.pop(best_station)
+    final_station.append(best_station)
+
+print(final_station)
